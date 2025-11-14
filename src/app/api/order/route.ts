981450b6e5955ef68f017a1cadd080b1ece36c52
Noah Bucheon <noah.bucheton27@gmail.com>
2025-11-14 08:35:46 +0100

    message de ton choix

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -14,9 +14,8 @@ type CartItem = {
   image?: string | null;
 };
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
 
 export async function POST(req: Request) {
   try {
